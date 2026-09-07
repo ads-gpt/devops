a61143d2fac14971f6f341101c52b3fdbde75392
Adarsh <108751847+zs0c131y@users.noreply.github.com>
2026-09-07 23:03:58 +0530

    Add name and register number input with Show Details box

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-// JavaScript functionality will be added later.
-// The "Show Details" button behavior will be implemented in a feature branch.
+// Show the entered name and register number inside the empty box.
+var button = document.getElementById("showDetailsBtn");
+var output = document.getElementById("output");
+
+button.addEventListener("click", function () {
+  var name = document.getElementById("name").value;
+  var regno = document.getElementById("regno").value;
+
+  output.innerHTML =
+    "<p>Name: " + name + "</p>" +
+    "<p>Register Number: " + regno + "</p>" +
+    "<p>Programme: MCA</p>";
+});
